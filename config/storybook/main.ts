25439ebc21aab9c2206e13d0d44a3738facb8bf2
Andrey_Loginov <myhomemail2015@mail.ru>
2025-01-17 18:26:47 +0500

    Commit #14.10.2. Fix errors in storybook/ main.ts and StyleDecorator.ts

--- config/storybook/main.ts
+++ config/storybook/main.ts
@@ -1,15 +1,15 @@
-/* 
-    * Ошибка "React is not defined." в Storybook.
-    * Если вы используете новые версии Storybook, он по умолчанию использует компилятор SWC, 
-    * и вам нужно немного изменить конфигурацию main.ts. 
-    * https://stackoverflow.com/questions/74995855/storybook-canvas-referenceerror-react-is-not-defined
-    * The SWC compiler doesn't work with React.
-    * Если вы включили параметр SWC Builder в проекте на основе React и не импортируете React 
-    * в свои файлы явным образом, это может привести к сбою загрузки Storybook. SWC не импортирует 
-    * модуль автоматически при использовании компоновщика SWC. Чтобы решить эту проблему, вам необходимо 
-    * настроить файл конфигурации Storybook.
-    * https://storybook.js.org/docs/configure/integration/compilers#the-swc-compiler-doesnt-work-with-react
-*/
+/*
+ * Ошибка "React is not defined." в Storybook.
+ * Если вы используете новые версии Storybook, он по умолчанию использует компилятор SWC,
+ * и вам нужно немного изменить конфигурацию main.ts.
+ * https://stackoverflow.com/questions/74995855/storybook-canvas-referenceerror-react-is-not-defined
+ * The SWC compiler doesn't work with React.
+ * Если вы включили параметр SWC Builder в проекте на основе React и не импортируете React
+ * в свои файлы явным образом, это может привести к сбою загрузки Storybook. SWC не импортирует
+ * модуль автоматически при использовании компоновщика SWC. Чтобы решить эту проблему, вам необходимо
+ * настроить файл конфигурации Storybook.
+ * https://storybook.js.org/docs/configure/integration/compilers#the-swc-compiler-doesnt-work-with-react
+ */
 
 import type { StorybookConfig } from '@storybook/react-webpack5';
 
@@ -30,7 +30,7 @@ const config: StorybookConfig = {
         name: '@storybook/react-webpack5',
         options: {},
     },
-    swc: (config, options) => ({
+    swc: () => ({
         jsc: {
             transform: {
                 react: {
--- src/shared/config/storybook/StyleDecorator/StyleDecorator.ts
+++ src/shared/config/storybook/StyleDecorator/StyleDecorator.ts
@@ -1,5 +1,4 @@
 import { StoryFn } from '@storybook/react/*';
-import React from 'react';
 import 'app/styles/index.scss';
 
 export const StyleDecorator = (story: () => StoryFn) => story();
